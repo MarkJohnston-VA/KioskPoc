4dbcc57d615592e22563b3d33593c31e74ab956c
Mark Johnston (VA) <mark.johnston@va.gov>
2025-09-19 11:54:16 -0500

    Demo1 (#6)
    * Added localsettings.env to gitignore.
    
    * Added functions
    
    * Added divide

--- utilities.js
+++ utilities.js
@@ -4,3 +4,24 @@ function add(number1, number2) {
 
 const sum = add(5, 10);
 console.log(sum);
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+const difference = subtract(10, 5);
+console.log(difference);
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+const product = multiply(5, 10);
+console.log(product);
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+const result = divide(20, 4);
+console.log(result);
+
